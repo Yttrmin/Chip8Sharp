85010211b8af0ca7f1338a1dff42d138b713de8a
Yttrmin@users.noreply.github.com <Yttrmin@users.noreply.github.com>
2014-04-07 11:34:46 -0400

    * Initial commit.

---
+++ Chip8Sharp/CPU.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Sharp
+{
+    class CPU
+    {
+        
+
+        private readonly bool[] GraphicsMemory;
+        private readonly byte[] Memory;
+        private readonly ushort[] Stack;
+        /// <summary>
+        /// Registers, V0 through VF, each 8 bits in size.
+        /// </summary>
+        private readonly byte[] V;
+        /// <summary>
+        /// Index register, 16 bits in size.
+        /// </summary>
+        private byte I;
+        private ushort PC;
+        private ushort SP;
+        private ushort IP;
+
+        private const int RegisterCount = 16;
+        private const int MemorySize = 4096;
+        private const int GraphicsMemorySize = 64 * 32;
+        private const int StackSize = 16;
+
+        public CPU()
+        {
+            this.V = new byte[RegisterCount];
+            this.Memory = new byte[MemorySize];
+            this.GraphicsMemory = new bool[GraphicsMemorySize];
+            
+        }
+
+        private void ConstructOpcodeMaps()
+        {
+
+        }
+
+        public void LoadMemory(byte[] Contents, int StartAddress)
+        {
+            Array.Copy(Contents, 0, this.Memory, StartAddress, Contents.Length);
+        }
+
+        /// <summary>
+        /// Fetches the opcode at Memory[PC] and increments the PC by 2.
+        /// </summary>
+        /// <returns>Opcode.</returns>
+        private ushort Fetch()
+        {
+            var Result = BitConverter.ToUInt16(this.Memory, this.PC);
+            PC += 2;
+            return Result;
+        }
+
+        private void DecodeExecute(ushort Instruction)
+        {
+
+        }
+    }
+}
---
+++ Chip8Sharp/Interpreter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Sharp
+{
+    class Interpreter
+    {
+        private delegate void Executor();
+        private readonly Dictionary<ushort, Executor> BaseOpcodeMap;
+        private readonly CPU CPU;
+
+        public Interpreter(CPU CPU)
+        {
+            this.CPU = CPU;
+            this.BaseOpcodeMap = new Dictionary<ushort, Executor>();
+        }
+
+        public void DecodeExecute(ushort Instruction)
+        {
+
+        }
+    }
+}
